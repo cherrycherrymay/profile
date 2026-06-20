a2a5f81f3398b3d54e2f0942cfae21dbe27a5cd7
maysa <maysa.s.alghamdi@gmail.com>
2026-06-20 12:39:37 +0300

    Display: Minro Changes

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ const CONTENT = {
 
   about: {
     text: {
-      en: "This Maysa AlGhamdi — a computer science graduate from JIC, currently working as a cybersecurity analyst, building software and offensive security tools on the side."
+      en: "This is Maysa AlGhamdi, a cybersecurity analyst with a Bachelor's degree in Computer Science, building software and offensive security tools on the side.
+"
     },
     Skills: ["Software Development", "Software Testing", "Reverse Engineering", "CI/DC", "Secure Coding" ],
     Tools: ["AWS", "Swagger", "Terraform", "Git"],
